9861c69b6998ee54d1adc1c3418c5f9c7fec8c30
Sandeep Raveesh <crsandy@gmail.com>
2016-11-28 00:15:47 -0800

    cloud code test

--- cloud/main.js
+++ cloud/main.js
@@ -1,43 +1,22 @@
-// Android push test
 Parse.Cloud.define('pushChannelTest', function(request, response) {
 
-  // request has 2 parameters: params passed by the client and the authorized user
-  var params = request.params;
-  var user = request.user;
+    var params = request.params;
+    var user = request.user;
+    var pushQuery = new Parse.Query(Parse.Installation);
+    pushQuery.equalTo("deviceType", "android");
 
-  // To be used with:
-  // https://github.com/codepath/ParsePushNotificationExample
-  // See https://github.com/codepath/ParsePushNotificationExample/blob/master/app/src/main/java/com/test/MyCustomReceiver.java
-  var customData = params.customData;
-  var launch = params.launch;
-  var broadcast = params.broadcast;
+    Parse.Push.send({
+        where: pushQuery,
+        data: "Just a test",
+    }, {
+        success: function() {
+            console.log("#### PUSH OK");
+        },
+        error: function(error) {
+            console.log("#### PUSH ERROR" + error.message);
+        },
+        useMasterKey: true
+    });
 
-  // use to custom tweak whatever payload you wish to send
-  var pushQuery = new Parse.Query(Parse.Installation);
-  pushQuery.equalTo("deviceType", "android");
-
-  var payload = {};
-
-  if (customData) {
-      payload.customdata = customData;
-  }
-  else if (launch) {
-      payload.launch = launch;
-  }
-  else if (broadcast) {
-      payload.broadcast = broadcast;
-  }
-
-  // Note that useMasterKey is necessary for Push notifications to succeed.
-
-  Parse.Push.send({
-  where: pushQuery,      // for sending to a specific channel
-  data: payload,
-  }, { success: function() {
-     console.log("#### PUSH OK");
-  }, error: function(error) {
-     console.log("#### PUSH ERROR" + error.message);
-  }, useMasterKey: true});
-
-  response.success('success');
+    response.success('success');
 });

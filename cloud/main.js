cde4c7cd0a0145b397bd5464a6fff24de3dfa5e7
Sandeep Raveesh <crsandy@gmail.com>
2016-11-28 00:37:28 -0800

    cloud code test

--- cloud/main.js
+++ cloud/main.js
@@ -15,12 +15,12 @@ Parse.Cloud.define('pushChannelTest', function(request, response) {
     }, {
         success: function() {
             console.log("#### PUSH OK");
+            response.success('parse server success');
         },
         error: function(error) {
             console.log("#### PUSH ERROR" + error.message);
+            response.success('parse server error', error);
         },
         useMasterKey: true
     });
-
-    response.success('success');
 });

b3a1cf07be1fdfaf838697ffa25906ab9d45aa96
Sandeep Raveesh <crsandy@gmail.com>
2016-12-06 20:47:41 -0800

    price push notification

--- cloud/main.js
+++ cloud/main.js
@@ -45,31 +45,38 @@ Parse.Cloud.define('pricePush', function(request, response) {
     console.log("store:", store);
     console.log("productName:", productName);
 
-    var Product = Parse.Object.extend("Product");
-    var prod = new Product();
-    prod.id = productId;
 
-    var userQuery = new Parse.Query(Parse.User);
-    userQuery.equalTo("wishListProducts", prod);
 
-    var pushMessage = 'Hurry up: ' + store + ' has ' + productName + ' for ' + price;
-
-    Parse.Push.send({
-        where: userQuery,
-        data: {
-            alert: pushMessage,
-            badge: 1,
-            sound: 'default'
-        }
-    }, {
-        success: function() {
-            console.log('##### PUSH OK');
-            response.success('success sent notifications');
-        },
-        error: function(error) {
-            console.log('##### PUSH ERROR');
-            response.error(error);
-        },
-        useMasterKey: true
+    var P1 = Parse.Object.extend("Product");
+    var p1q = new Parse.Query(P1);
+    query.get(productId, {
+      success: function(pout) {
+          console.log("product id from query is" + pout.id);
+          var userQuery = new Parse.Query(Parse.User);
+          userQuery.equalTo("wishListProducts", pout);
+          var pushMessage = 'Hurry up: ' + store + ' has ' + productName + ' for ' + price;
+          Parse.Push.send({
+              where: userQuery,
+              data: {
+                  alert: pushMessage,
+                  badge: 1,
+                  sound: 'default'
+              }
+          }, {
+              success: function() {
+                  console.log('##### PUSH OK');
+                  response.success('success sent notifications');
+              },
+              error: function(error) {
+                  console.log('##### PUSH ERROR');
+                  response.error(error);
+              },
+              useMasterKey: true
+          });
+      },
+      error: function(object, error) {
+        console.log("error no product found");
+        console.log('##### PUSH ERROR');
+      }
     });
 });
